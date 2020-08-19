a887ed6863e129677ced0d19d3e06baa9235b47a
mg.kim <mg.kim@linecorp.com>
2020-08-19 23:51:53 +0900

    First pass at working Add Cheese form

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from autoslug import AutoSlugField
 from model_utils.models import TimeStampedModel
 from django_countries.fields import CountryField
+from django.urls import reverse
 
 class Cheese(TimeStampedModel):
     name = models.CharField("Name of Cheese", max_length=255)
@@ -22,4 +23,7 @@ class Cheese(TimeStampedModel):
 
     def __str__(self):
         return self.name        
+
+    def get_absolute_url(self):
+        return reverse('cheeses:detail', kwargs={"slug": self.slug})
     
